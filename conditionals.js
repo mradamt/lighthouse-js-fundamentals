efb833e801600693cc06f766205d016f08673e39
adam <mr.adamtyler@gmail.com>
2020-08-25 19:45:27 +0000

    Use conditionals to tell users about the weather

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+const temp = 15;
+const raining = false;
+const cold = true;
+
+if (temp < 0) {
+  console.log("Temp is freezing");
+} else if (temp < 16) {
+  console.log("Temp is mild, take a scarf");
+} else {
+  console.log("It's warmer than 16 deg");
+}
+
+if (raining) {
+  console.log("Don't forget your waterproof hair");
+} else {
+  console.log("No rain");
+}
+
+if (cold) {
+  console.log("Is cold");
+} else {
+  console.log("Sun's out guns out");
+}
+
+console.log("Now go outside");
