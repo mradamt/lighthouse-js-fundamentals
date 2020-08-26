3d061683437e90e7b053d427676740449a83bf4e
adam <mr.adamtyler@gmail.com>
2020-08-26 22:39:09 +0000

    Program selects valid voting locations from an array of possible locations

---
+++ voting.js
@@ -0,0 +1,18 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+function chooseStations (stations) {
+  let validStations = [];
+  for (const station of stations) {
+    if (station[1] >= 20 && (station[2] === "school" || station[2] === "community centre")) {
+      validStations.push(station[0]);
+    }
+  }
+  return validStations;
+}
+
+chooseStations(stations)
+// console.log(chooseStations(stations));
