d9b5f3ecf2d7c5230d2d5bf5ade16fbbe9d5eb1a
adam <mr.adamtyler@gmail.com>
2020-08-27 21:37:20 +0000

    Program updted to use while loop to print kitchen pack list

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
-  console.log(packingList[i]);
-}
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
 
+let i = 0;
+let len = packingList.length;
+while (i < len) {
+  console.log(packingList[i]);
+  i++;
+}
