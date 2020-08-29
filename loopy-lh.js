7c6896f4704220ace4219f49da5eca19a2b7d08f
Adam Tyler <mr.adamtyler@gmail.com>
2020-08-29 15:53:50 -0700

    Refactor program into a function to accept parameters
    Parameters accepted: range (array of two numbers, start and end of range), multiples (array of two numbers, multiples of which will be replaced by words), words (two words that'll be inserted in place of the numbers being replaced)

--- loopy-lh.js
+++ loopy-lh.js
@@ -1,13 +1,28 @@
-let n = 100;
 
-for (n = 100; n <= 200; n++) {
-  if (n % 3 === 0 && n % 4 === 0) {
-    console.log("LoopyLighthouse");
-  } else if (n % 3 === 0) {
-    console.log("Loopy");
-  } else if (n % 4 === 0) {
-    console.log("Lighthouse");
-  } else {
-    console.log(n);
+const loopyLighthouse = function (range, multiples, words) {  
+  // Parse out variables from function arguments
+  const start = range[0];
+  const end = range[1];
+
+  const multi1 = multiples[0];
+  const multi2 = multiples[1];
+
+  const replace1 = words[0];
+  const replace2 = words[1];
+  const replace3 = replace1 + replace2;
+
+  // Execute loops using variable names, to save fresh calculation every loop
+  for (let n = start; n <= end; n++) {
+    if (n % multi1 === 0 && n % multi2 === 0) {
+      console.log(replace3);
+    } else if (n % multi1 === 0) {
+      console.log(replace1);
+    } else if (n % multi2 === 0) {
+      console.log(replace2);
+    } else {
+      console.log(n);
+    }
   }
 }
+
+loopyLighthouse([15, 30], [2, 5], ["Batty", "Beacon"])
