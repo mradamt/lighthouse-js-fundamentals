20513f3e4f819d7f322e474fd7a5123057d1ac3c
Adam Tyler <mr.adamtyler@gmail.com>
2020-10-23 09:33:27 -0700

    shortened code by making use of trim() instead of doing this operation manually

--- katas/percent-encoded-string.js
+++ katas/percent-encoded-string.js
@@ -1,30 +1,20 @@
 // secretGist name: lhl-katas--percent-encoded-string
 /* urlEncode converts all whitespace ' ' chars in a given string to '%20', for use in urls. 
- * it ignores all leading/trailing whitespace. */
+ * it trims all leading/trailing whitespace. */
 
 const urlEncode = function(text) {
-  let textLen = text.length
+  // Trim leading/trailing whitespace (using .trim())
+  let trimText = text.trim();
+  let textLen = trimText.length
   let encodedText = "";
 
-  // First loop: remove all trailing whitespace chars by resetting 'textLen' to the final non-whitespace char
-  for (let j = textLen; j >= 0; j--) {
-    if (text[j-1] == " ") {
-      continue;
-    } else {
-      textLen = j;
-      break;
-    }
-  }
-
-  /* Second loop: first condition ignores all leading whitespace chars, 
-   * then either replace a whitespace char with '%20' or copy the 'text' char to the 'encodedText' variable */
+  /* Loop through the trimmed text char by char, if the character is whitespace then
+   * append '%20' to the encodedText string, otherwise append the character as-is. */
   for (let i = 0; i < textLen; i++) {
-    if (text[i] == " " && encodedText == "") {
-      continue
-    } else if (text[i] == " ") {
+    if (trimText[i] === " ") {
       encodedText += "%20";
     } else {
-      encodedText += text[i];
+      encodedText += trimText[i];
     }
   }
 
