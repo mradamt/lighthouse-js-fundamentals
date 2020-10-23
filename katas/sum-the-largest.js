27de650634e5ce52a677ddbc5036a54bf6e252a4
Adam Tyler <mr.adamtyler@gmail.com>
2020-10-23 14:31:09 -0700

    corrected error that miscaluclated sum if data[1] was less than data[0]

--- katas/sum-the-largest.js
+++ katas/sum-the-largest.js
@@ -4,8 +4,8 @@
 const sumLargestNumbers = function(data) {
   // Set two variables equal to the first two numbers in the array 
   // (Doing this instead of initializing to 0 or 'null' means the function deals with negative numbers correctly)
-  let largest = data[0];
-  let secondLargest = data[1];
+  let largest = data[1];
+  let secondLargest = data[0];
   
   // Iterate through array: cascading the two largest numbers into the respective variables
   data.forEach(function (thisNum) {
@@ -21,7 +21,8 @@ const sumLargestNumbers = function(data) {
   return largest + secondLargest
 };
 
-console.log(sumLargestNumbers([1, 10, 5, 5, 4, 12, 1]));
-console.log(sumLargestNumbers([1, 2, 3]));
-console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2, -1, ]));
-console.log(sumLargestNumbers([-10, -44, -1, -3, 5, -20]))
+// console.log(sumLargestNumbers([1, 10, 5, 5, 4, 12, 1]));
+// console.log(sumLargestNumbers([1, 2, 3]));
+// console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2, -1, ]));
+// console.log(sumLargestNumbers([-10, -44, -1, -3, 5, -20]))
+console.log(sumLargestNumbers([4, -2]))
